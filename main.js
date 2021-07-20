29b0d2b95bfe746f5d201383e4de05a32641324d
xylophonezygote <xylophonezygote@gmail.com>
2021-07-20 17:09:42 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// Orgs
+
+/* CommunityXYZ
+Verto
+DecentLand
+ArWiki
+ArVerify
+ArConnect
+Gitcoin
+Evermore
+ArGo
+Koii
+Mintable
+Mirror
+$QUAD
+CheckMyNft
+Nest.land
+Sarcophagus
+Weve
+KYVE */
+
+// Categories
+
+/* PSC
+Exchange
+Social
+Publishing
+Identity
+Developer tools
+File storage
+NFT
+Productivity tools */
